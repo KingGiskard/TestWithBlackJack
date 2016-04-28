cb39490b62ba56c23d495d7481367562775e518f
KingGiskard <bobby.james.king@gmail.com>
2016-04-27 22:03:37 -0600

    made changes

---
+++ Card.java
@@ -0,0 +1,4 @@
+public class Card
+{
+//This will be used in order to develop a card class for the game
+}
--- newBlackJackStuff.java
+++ newBlackJackStuff.java
@@ -3,5 +3,6 @@ public class BlackJack
   public staic void main(String[]args)
   {
     System.out.println("THis is a test !");
+    System.out.println("this is super fun too");
   }
 }
