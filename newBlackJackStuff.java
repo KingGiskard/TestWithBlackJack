7bb6fd1d3a319db96f7ee736914a20ac7ad98af2
KingGiskard <bobby.james.king@gmail.com>
2016-04-27 22:35:28 -0600

    there

--- newBlackJackStuff.java
+++ newBlackJackStuff.java
@@ -5,10 +5,5 @@ public class BlackJack
     System.out.println(" conflict today !");
     System.out.println("this is super fun too");
     System.out.println("add more stuff");
-<<<<<<< HEAD
-    here is more stuff
-=======
-    add more stuff
->>>>>>> eef993bd09d0dd042bdf15a92e6ff8d1eb7249f7
   }
 }
