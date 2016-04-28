e81bc3d6b06975767f1faf0cebca6e33310a44d0
KingGiskard <bobby.james.king@gmail.com>
2016-04-27 22:11:22 -0600

    add more words

--- newBlackJackStuff.java
+++ newBlackJackStuff.java
@@ -2,7 +2,7 @@ public class BlackJack
 {
   public staic void main(String[]args)
   {
-    System.out.println("THis is a merge conflict !");
+    System.out.println("THis is a how will i fix this conflict !");
     System.out.println("this is super fun too");
   }
 }
