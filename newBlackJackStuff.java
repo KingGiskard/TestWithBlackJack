21fdff4ce1a146164cc18d637772d9eadc011fdd
KingGiskard <bobby.james.king@gmail.com>
2016-04-27 22:17:33 -0600

    change conflict

--- newBlackJackStuff.java
+++ newBlackJackStuff.java
@@ -2,7 +2,7 @@ public class BlackJack
 {
   public staic void main(String[]args)
   {
-    System.out.println("I wil create a conflict !");
+    System.out.println(" conflict today !");
     System.out.println("this is super fun too");
   }
 }
