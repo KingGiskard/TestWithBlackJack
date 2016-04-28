9c4a68746e7573599343979ce2c2651925216a7b
KingGiskard <bobby.james.king@gmail.com>
2016-04-27 22:09:34 -0600

    created a merge Conflict

--- newBlackJackStuff.java
+++ newBlackJackStuff.java
@@ -2,7 +2,7 @@ public class BlackJack
 {
   public staic void main(String[]args)
   {
-    System.out.println("THis is a test !");
+    System.out.println("THis is a merge conflict !");
     System.out.println("this is super fun too");
   }
 }
